18645ec244ddc015adf0beccff96c085dba13d1b
pwkad <patrickwaltersc21@yahoo.com>
2019-02-08 21:04:08 -0600

    woops

--- client-applications/client/src/resources/value-converters/value-in-usd.js
+++ client-applications/client/src/resources/value-converters/value-in-usd.js
@@ -1,16 +1,7 @@
-import {Session} from '../../services/session';
-
 export class ValueInUsdValueConverter {
   static inject = [Session];
-  constructor(session) {
-    this.session = session;
-  }
 
   toView(value, lastExchangeRate) {
-    if (!lastExchangeRate) {
-      lastExchangeRate = this.session.currentExchangeRate.last;
-    }
-
     const dollarsPerSatoshi = lastExchangeRate / 100000000;
     const satoshis = value;
 
